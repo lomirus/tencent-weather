c08f0aeb923cd8d75f574cb0eb69bce629774dea
Rhys <lomirus@qq.com>
2021-05-29 22:47:06 +0800

    chore: restore the sw

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,12 @@ import './styles/main.css'
 const app = document.querySelector("#app");
 ReactDOM.render(<App />, app);
 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('/service-worker.js').then(registration => {
-//             console.log('SW registered: ', registration);
-//         }).catch(registrationError => {
-//             console.log('SW registration failed: ', registrationError);
-//         });
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
