91da2757ac0841412900c44d15897e73a61eff64
Rhys <lomirus@qq.com>
2021-05-29 15:24:04 +0800

    feat: styles of suggestions

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -5,7 +5,31 @@ import { ReducerAction } from '../store/types'
 
 const styles: Record<string, CSSProperties> = {
     main: {
-        
+        marginTop: "10px",
+        display: "flex",
+        flexDirection: "column",
+        height: "200px",
+        flexWrap: "wrap",
+        backgroundColor: "#FFF"
+    },
+    item: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "98px",
+        width: "calc(25vw - 2px)",
+        border: "1px solid #e6e6e6"
+    },
+    icon: {
+        margin: "15px 0 10px 0"
+    },
+    state: {
+        fontSize: "14px",
+        color: "#434343",
+    },
+    for: {
+        color: "#999",
+        fontSize: "12px"
     }
 }
 
@@ -19,10 +43,10 @@ const Suggestions = ({}: PropsType) => {
         fetchSuggestions(dispatch)
     })
     return <div style={styles.main}>
-        {state.suggestions.map(s => (<div key={s.for}>
-            <img src={require(`../assets/suggestions/${s.icon}.svg`).default} />
-            <span>{s.state}</span>
-            <span>{s.for}</span>
+        {state.suggestions.map(s => (<div key={s.for} style={styles.item}>
+            <img style={styles.icon} src={require(`../assets/suggestions/${s.icon}.svg`).default} />
+            <span style={styles.state}>{s.state}</span>
+            <span style={styles.for}>{s.for}</span>
         </div>))}
     </div>
 }
